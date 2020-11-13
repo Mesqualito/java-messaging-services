cd19e13c66f3ec89682050bf5522218a1bbd2538
Mesqualito <jochen@gebsattel.rocks>
2020-11-13 13:18:04 +0100

    Project setup with Spring Boot and additionally the embedded Apache ActiveMQ Artemis server

---
+++ src/main/java/com/eigenbaumarkt/javamessagingservices/JavaMessagingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.eigenbaumarkt.javamessagingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessagingServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMessagingServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eigenbaumarkt/javamessagingservices/JavaMessagingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eigenbaumarkt.javamessagingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagingServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
